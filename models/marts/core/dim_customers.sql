8e1dfb08247f546a9fb3a221120c122bc31bf94c
Andrew Merrick <drew.merrick@gmail.com>
2023-07-14 03:18:21 +0000

    synching from the exemplar

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,49 +1,34 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
+with
 
-with customers as (
-
-    select * from {{ ref ('stg_customers')}}
+customers as (
+    select * from {{ ref('stg_customers')}}
 ),
 
 orders as (
-    select * from {{ ref ('stg_orders')}}
+    select * from {{ ref('fct_orders')}}
 ),
 
-payments as (
-    select * from {{ ref ('stg_payments')}}
-)
-,
 customer_orders as (
-
     select 
         customer_id,
-        min(orders.order_date) as first_order_date,
-        max(orders.order_date) as most_recent_order_date,
-        count(orders.order_id) as number_of_orders,
-        sum(payments.payment_amount) as lifetime_value
-
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-    left join payments using(order_id)
     group by customer_id
 ),
 
 final as (
-
-    select 
+    select
         customers.customer_id,
         customers.first_name,
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        coalesce(customer_orders.lifetime_value, 0)
-
+        customer_orders.lifetime_value
     from customers
     left join customer_orders using (customer_id)
 )
-
 select * from final
